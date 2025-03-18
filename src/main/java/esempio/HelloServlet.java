3f9c63f714e0f2f0890c9bdff04cf68504841463
Gius30 <giuseppe.nutricato@gmail.com>
2025-03-18 11:15:14 +0100

    modificato

--- src/main/java/esempio/HelloServlet.java
+++ src/main/java/esempio/HelloServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloServlet
  */
-@WebServlet("/HelloServlet")
+@WebServlet("/Hello")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
